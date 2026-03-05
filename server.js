9fc00d7ba1aa21ddc43399f5516afb48cf0ba5c5
Pranjal Sharma <pranjal.2428cseai762@kiet.edu>
2026-03-05 20:02:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -45,6 +45,4 @@ app.get("/delete", async(req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log("Server running on port " + PORT);
-});
+app.listen(3000);
